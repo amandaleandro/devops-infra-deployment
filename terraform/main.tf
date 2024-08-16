0b49c6caa247c86630b0cf5ebb03de3d8561d9ef
Amanda Leandro Soares Do Carmo <amandaleandrosoares@gmail.com>
2024-08-16 00:52:34 -0300

    fix:correcao da esteira

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,52 +3,43 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "/modules/vpc"
 }
 
 module "ec2" {
-  source = "./modules/ec2"
+  source = "/modules/ec2"
   vpc_id = module.vpc.vpc_id
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source = "/modules/eks"
   vpc_id = module.vpc.vpc_id
 }
 
 module "rds" {
-  source      = "./modules/rds"
+  source      = "/modules/rds"
   vpc_id      = module.vpc.vpc_id
   db_password = var.db_password
 }
 
 module "secrets_manager" {
-  source             = "./modules/secrets"
-  db_password        = var.db_password
-  aws_access_key_id  = "your_aws_access_key_id_here"
-  secret_access_key  = "your_secret_access_key_here"
-}
-
-variable "secret_access_key" {
-  description = "Secret access key for AWS"
-  type        = string
-  default     = "your_default_secret_access_key_here"
-}
+  source = "modules/secrets"
+  }
 
 module "cdn" {
   source = "./modules/cdn"
 }
 
 module "observability" {
-  source = "./modules/observability"
+  source = "/modules/observability"
 }
 
 module "auto_scaling" {
-  source = "./modules/auto_scaling"
+  source = "/modules/auto_scaling"
 }
 
 module "wordpress" {
-  source    = "./modules/wordpress"
+  source    = "/modules/wordpress"
   vpc_id    = module.vpc.vpc_id
   subnet_id = module.ec2.public_subnet_id
   db_password = var.db_password
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,24 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+variable "db_password" {
+  description = "Password for the RDS database"
+  type        = string
+  default     = "your_default_password_here"  # Defina a senha do banco de dados aqui
+}
+
+variable "version" {
+  description = "Version of the deployment"
+  type        = string
+  default     = "1.0.0"
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
